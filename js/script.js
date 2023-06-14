03ad844ce10b401ebfef077af6a2433dcfa1d473
Muhammad Alif <contact.alif02@gmail.com>
2023-06-15 00:18:56 +0700

    add projects, contact, footer

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ window.addEventListener('DOMContentLoaded', function () {
   const nav = document.querySelector('#nav');
   nav.scrollIntoView({ behavior: 'smooth' });
 });
+
+window.addEventListener('load', function () {
+  const form = document.getElementById('message-from-portfolio');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    }).then(() => {
+      alert('Thanks for hitting me up! ;D');
+    });
+  });
+});
